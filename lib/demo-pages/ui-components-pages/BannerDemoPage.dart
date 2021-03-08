96aae8707f35ede0eb64d0b2bd27f46971b2e0c2
tanghao <tanghao@C12583.local>
2021-03-08 19:25:03 +0800

    feat: bannerComponent

--- lib/demo-pages/ui-components-pages/BannerDemoPage.dart
+++ lib/demo-pages/ui-components-pages/BannerDemoPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../../ui-components/Banner.dart';
 
 class BannerDemoPage extends StatefulWidget {
   BannerDemoPage({Key key}) : super(key: key);
@@ -11,10 +12,32 @@ class _BannerDemoPageState extends State<BannerDemoPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('BannerDemoPage'),
-      ),
-      body: Text('BannerDemoPage'),
-    );
+        appBar: AppBar(
+          title: Text('BannerDemoPage'),
+        ),
+        body: ListView(
+          children: [
+            CodemaoBanner(
+              showBottom: true,
+              axis: Axis.horizontal,
+            ),
+            Text('水平显示标识'),
+            CodemaoBanner(
+              showBottom: false,
+              axis: Axis.horizontal,
+            ),
+            Text('水平不显示标识'),
+            CodemaoBanner(
+              showBottom: true,
+              axis: Axis.vertical,
+            ),
+            Text('垂直显示标识'),
+            CodemaoBanner(
+              showBottom: false,
+              axis: Axis.vertical,
+            ),
+            Text('垂直不显示标识'),
+          ],
+        ));
   }
 }
--- lib/ui-components/Banner.dart
+++ lib/ui-components/Banner.dart
@@ -1,20 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
-class CodemaoBanner extends StatefulWidget {
-  CodemaoBanner({Key key}) : super(key: key);
+class CodemaoBanner extends StatelessWidget {
+  final Axis axis;
+  final bool showBottom;
+  final List<String> images = [
+    'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3395864961,1527657332&fm=26&gp=0.jpg',
+    'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3395864961,1527657332&fm=26&gp=0.jpg',
+    'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3395864961,1527657332&fm=26&gp=0.jpg',
+    'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3395864961,1527657332&fm=26&gp=0.jpg',
+  ];
+  CodemaoBanner({Key key, this.axis, this.showBottom, List<String> images})
+      : super(key: key);
 
-  @override
-  _CodemaoBannerState createState() => _CodemaoBannerState();
-}
-
-class _CodemaoBannerState extends State<CodemaoBanner> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Banner'),
+    return Container(
+      height: 180,
+      child: Swiper(
+        itemBuilder: (BuildContext context, int index) {
+          return Image.network(
+            images[index],
+            fit: BoxFit.fill,
+          );
+        },
+        autoplay: true,
+        itemCount: images.length,
+        pagination: showBottom ? SwiperPagination() : SwiperPagination(),
+        scrollDirection: axis,
+        // control: new SwiperControl(),
       ),
-      body: Text('codemaoBanner组件'),
     );
   }
 }
